5f98c1a0c6f81f1fa16ad89a950c9d0456721aca
William Annis <wannis@fsu.edu>
2026-07-09 12:03:51 -0400

    Updated work flow to accommodate species without group specific growth parameters

--- R_scripts/03_igr_production.R
+++ R_scripts/03_igr_production.R
@@ -23,10 +23,12 @@ devtools::load_all("~/Documents/work/R packages/secProd")
 
 # Production directories
 input_dir <- "input_data"
+export_dir <- "prod_data"
 
 # Growth model directories
 grow_dir <- "~/Documents/Work/Everglades post-doc/Data analysis/growth curves"
 stack_dir <- file.path(grow_dir,"loo_outputs_cat")
+stackJ_dir <- file.path(grow_dir,"loo_outputs")
 mod_dir <- file.path(grow_dir,"stan_outputs/model_out")
 
 # Data
@@ -34,6 +36,7 @@ len_df <- readRDS(file.path(input_dir,"fslen_imputed_2026-07-09.rds"))
 hyd_df <- readRDS(file.path(input_dir,"hydr_class_annual_2026-07-09.rds"))
 wt_df <- read.csv(file.path(input_dir,"length_weight_parameters.csv"))
 stack_list <- readRDS(file.path(stack_dir,"stack_wt_out_2026-06-22.rds"))
+stackJ <- readRDS(file.path(stackJ_dir,"stack_wt_out_2026-06-22.rds"))["JORFLO"]
 
 
 # Data preparation  ------------------------------------------------------------
@@ -87,36 +90,55 @@ bio_df <- len_df %>%
     length,
     wt
     )
-  
+
 
 # Species-specific production estimates  ---------------------------------------
-sp <- names(stack_list)[[4]]
-cohort <- 30
 
-prod_list <- lapply(sp, function(sp){
+# JORFLO has no group speficic growth esitmates and stacking wts are contained 
+# in seperate file. Adds these to main stacking weigth list
+stack_list <- c(stack_list,stackJ)
+
+# Production input settings
+sp <- names(stack_list)
+cohort <- 30
+growth_iter <- 10
+prod_iter <- 10
+
+prod_list <- lapply(sp, function(s){
   
   # Filter all data for one species
-  bio <- bio_df %>% filter(species == sp)
-  stack <- stack_list[[sp]]
-  sp_dir <- file.path(mod_dir,sp)
-  wt <- wt_df %>% filter(species == sp)
+  samp <- samp_df
+  bio <- bio_df %>% filter(species == s)
+  stack <- stack_list[[s]]
+  sp_dir <- file.path(mod_dir,s)
+  wt <- wt_df %>% filter(species == s)
+  group_type = "cat"
+  pred_group <- interval_df$group_id
+  pred_interval <- interval_df$interval
   
   # Create species specific length input data
   length_vec <- min(bio$length,na.rm = T):max(bio$length,na.rm = T)
+  
+  # JORFLO does not have group specific growth rates, use population
+  if(s == "JORFLO") {
+    group_type <- "mu"
+    pred_group <- NULL
+    samp <- samp %>% mutate(group_id = 1)
+  }
   
   # Species specific growth and age at length estimation
   growth_post <- stack_predict(
     stack.df = stack,
     mod.dir = sp_dir,
-    sim = 10,
+    sim = growth_iter,
     summarize = F,
     sum.fun = "median",
     type = "prediction",
-    group.id = "cat",
+    group.id = group_type,
     pred.input = length_vec,
     create.input = T,
-    pred.group = interval_df$group_id,
-    pred.interval = interval_df$interval,
+    pred.group = pred_group,
+    pred.interval = pred_interval,
     stack = T,
     input.var = "length",
     output.var = c("interval_growth","age"),
@@ -129,26 +151,26 @@ prod_list <- lapply(sp, function(sp){
   # Species specific production
   production(
     method = "igr",
-    sample = samp_df,
+    sample = samp,
     biomass = bio,
     growth = growth_post, 
     class.type = "size",
     class.size = 1,
     bio.boot = T, 
     growth.boot = T,
-    iter = 10,
+    iter = prod_iter,
     return.raw = F,
     parallel = T,
     mc.cores = 10
-  )
-  
+  ) %>% 
+    mutate(species = s)
 })
-
-
+prod_df <- bind_rows(prod_list)
 
 
 # Export  ----------------------------------------------------------------------
-
+prod_file <- paste0("fsprod_igr_",Sys.Date(),".rds")
+saveRDS(prod_df,file.path(export_dir,prod_file))
 
 
 
