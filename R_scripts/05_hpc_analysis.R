765b5e45371e8fb1b4f57a0fed56aaf0a439d0f6
William Annis <wannis@fsu.edu>
2026-09-03 14:13:13 -0400

    streamline analysis script to only run model. data prep will now be handled in data prep script locally

--- R_scripts/05_hpc_analysis.R
+++ R_scripts/05_hpc_analysis.R
@@ -15,71 +15,15 @@
 rm(list = ls()) 
 
 # Load in packages
-library(dplyr)
 library(ssh)
 
 # directories
-mod_dir <- "stan_scripts"
-input_dir <- "prod_data"
-export_dir <- "hpc/stan_input"
+mod_dir <- "hpc_scripts"
+export_dir <- "hpc/stan_output"
 
-# Data
-prod_df <- 
-  readRDS(file.path(input_dir,"fsprod_igr_2026-07-13.rds"))
-phy_site <- 
-  readRDS(file.path(input_dir,"phys_site_predictors_2026-08-17.rds"))
-
-
-# Prep data  -------------------------------------------------------------------
-
-# Remove NA production estimates
-prod_for <- prod_df %>% filter(!is.na(production_mean))
-
-# Create a composite production measure using all species
-prod_all <- prod_for %>% 
-  group_by(site,cum,date,area,interval) %>% 
-  summarise(
-    across(contains("sample_den"),sum),
-    across(contains("biomass"),sum),
-    across(contains("production"),sum)
-  ) %>% 
-  mutate(species = "all") %>% 
-  bind_rows(prod_for) %>% 
-  mutate(
-    ptob = production_mean/interval_biomass_mean,
-    ptob = case_when(
-      is.nan(ptob) ~ 0,
-      T ~ ptob
-    )
-  )
-
-# Add sample info to production data
-samp_df <- phy_site %>% 
-  distinct(wateryear,region,site,cum) %>% 
-  filter(wateryear != 2024) %>%   ## TEMPORARY ASK NATE FOR NEWEST SHARK RIVER DATA (2025)
-  filter(wateryear !=1995) %>%  ## TEMP ASK JOEL FOR LAG DATA FOR THIS YEAR
-  left_join(prod_all, by = join_by(site,cum)) %>% 
-  filter(!is.na(production_mean)) %>% 
-  
-  # transform response varibales to improv convergence
-  mutate(
-    production_mean = production_mean*1000,
-    biomass_mean = biomass_mean*1000,
-    ptob = ptob*1000 
-  )
-
-# Number of species response combinations
-nrow(
-  expand.grid(
-    unique(samp_df$species),
-    c("production_mean","biomass_mean","ptob")
-    )
-  )
-# Use this value in batch array shell script
 
 # HPC configuration  -----------------------------------------------------------
 
-library(ssh)
 # Connect to cluster
 session <- ssh_connect("wka25@hpc-login.rcc.fsu.edu")
 
