2024507e83b233baf04ad829798f6696cd2c64ae
William Annis <wannis@fsu.edu>
2026-09-06 12:37:01 -0400

    switched from json to rds inputs. Added messages for slurm outputs. Added model convergence and sampling diagnositics

--- hpc/R_scripts/hpc_csi_analysis.R
+++ hpc/R_scripts/hpc_csi_analysis.R
@@ -2,15 +2,25 @@
 #   Run stan models using HPC cluster
 #-------------------------------------------------------------------------------
 
-message("Start model run script")
+message("============================================================")
+message("Starting hpc_cis_analysis.R")
+message("Start time: ", Sys.time())
+message("Slurm job ID: ", Sys.getenv("SLURM_JOB_ID"))
+message("Slurm array job ID: ", Sys.getenv("SLURM_ARRAY_JOB_ID"))
+message("Slurm array task ID: ", Sys.getenv("SLURM_ARRAY_TASK_ID"))
+message("============================================================")
 
 # For use with SBATCH on Slurm Scheduler. 
 # DO NO RUN OUTSIDE OF SLURM.
 
 # Housekeeping  ----------------------------------------------------------------
+rm(list = ls())
 
 # Load in packages
+message("[", Sys.time(), "] Loading packages...")
 library(cmdstanr)
+library(posterior)
+message("[", Sys.time(), "] Packages loaded")
 
 # directories
 mod_dir <- "hpc/stan_scripts"
@@ -22,37 +32,111 @@ out_dir <- "hpc/stan_outputs"
 # Shell argument for selecting species and response of choice
 arg <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
+message("[", Sys.time(), "] Selecting input file for array task ", arg)
 file_name <- list.files(
   input_dir,
-  pattern = "\\.json$",
+  pattern = "\\.rds$",
   full.names = TRUE
-  )[arg]
+)[arg]
+message("[", Sys.time(), "] Input file: ", file_name)
+
+# Load in data
+message("[", Sys.time(), "] Loading data...")
+data <- readRDS(file_name)
+message("[", Sys.time(), "] Data loaded")
 
 
 # Run models  ------------------------------------------------------------------
 
 # Load in model
-message("Compiling code")
-mod <- cmdstan_model()
+mod_name <- "tweedie_mvn_regyear_effects.stan"
+message("[", Sys.time(), "] Compiling/loading Stan model...")
+mod <- cmdstan_model(file.path(mod_dir,mod_name))
+message("[", Sys.time(), "] Stan model ready")
 
 # Run model
-message("Start stan model run")
+n_iter <- 2000
+n_warm <- 1000
+chains <- 4
+message("------------------------------------------------------------")
+message(paste0("[", Sys.time(), "] Starting Stan sampling for ", mod_name))
+message(paste0("Chains: ", chains))
+message(paste0("Warmup iterations: ", n_warm))
+message(paste0("Sampling iterations: ", n_iter))
+message("------------------------------------------------------------")
+
+start_time <- Sys.time()
+
 out <- mod$sample(
-  data = file_name,
-  iter_sampling = 2000, 
-  iter_warmup = 1000,
-  chains = 4,
+  data = data,
+  iter_sampling = n_iter, 
+  iter_warmup = n_warm,
+  chains = chains,
   parallel_chains = 4 
 )
-message("stan model run complete")
+
+end_time <- Sys.time()
+
+message("------------------------------------------------------------")
+message("[", end_time, "] Stan sampling complete")
+message("Sampling time: ", round(
+  difftime(end_time, start_time, units = "mins"), 2
+), " minutes")
+message("------------------------------------------------------------")
 
 
-# Export model  ----------------------------------------------------------------
+# Diagonstics  -----------------------------------------------------------------
+
+# Check for convergence or sampling issues
+message("[", Sys.time(), "] Checking convergence and sampling diagnostics...")
+
+# convergence
+conv <- out$summary(NULL, c("rhat", "ess_bulk"))
+high_rhat <- sum(conv$rhat > 1.1, na.rm = TRUE)
+low_ess   <- sum(conv$ess_bulk < 400, na.rm = TRUE)
+
+# sampling
+diag <- out$diagnostic_summary()
+div <- sum(diag$num_divergent)
+tree <- sum(diag$num_max_treedepth)
+
+# combined issues
+issues <- div + high_rhat + low_ess
+
+if (issue == 0) {
+  message("============================================================")
+  message("No issues dectected:")
+  message("  Rhat > 1.1: ", high_rhat)
+  message( "Bulk ESS < 400: ", low_ess)
+  message("  Divergent transitions: ", div)
+  message(" Max tree depth exceeded: ", tree)
+  message("============================================================")
+} else {
+  message("============================================================")
+  message("WARNING")
+  message("Issues detected:")
+  message("  Rhat > 1.1: ", high_rhat)
+  message("  Divergent transitions: ", div)
+  message(" Max tree depth exceeded: ", tree)
+  message("============================================================")
+}
+
+# Export  ----------------------------------------------------------------------
 
 # Name file and export
 out_name <-  file_name |>
   gsub("hpc/data",out_dir,x=_) |>
-  gsub("_input_data.json","_stan_out.rds",x=_)
-  
+  gsub("_input_data","_stan_out",x=_)
+
 saveRDS(out,out_name)
+message("[", Sys.time(), "] Model successfully saved:")
+message("  ", out_name)
+
+
+# End script  ------------------------------------------------------------------
+
+message("============================================================")
+message("Finished hpc_csi_analysis.R run")
+message("End time: ", Sys.time())
+message("============================================================")
 
