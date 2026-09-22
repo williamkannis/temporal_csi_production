f5ebc07820c6d244894b79113f992a50661ad037
William Annis <wannis@fsu.edu>
2026-09-22 08:38:09 -0400

    updated script to allow user to specify the response type. Now can run the tweedie or hurdle model based on response type

--- hpc/R_scripts/hpc_csi_analysis.R
+++ hpc/R_scripts/hpc_csi_analysis.R
@@ -29,46 +29,81 @@ input_dir <- "hpc/data"
 out_dir <- "hpc/stan_outputs"
 
 
-
 # Select specified dataset  ------------------------------------------------------
 
 # Shell job id for selecting species and response of choice
 task <- as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
 
-message("[", Sys.time(), "] Selecting input file for array task ", task)
+# Load in command arguments
+message("[", Sys.time(), "] Load in command arguments... ")
+arg <- commandArgs(trailingOnly = TRUE)
+if (length(arg) < 1) {
+  stop("Missing required arguments")
+}
+message("[", Sys.time(), "] Loaded in following arguments: ",arg)
+
+# Select response type
+message("[", Sys.time(), "] Selecting model response for arg ...")
+mod_arg <- arg[1]
+if(!mod_arg %in% c("sample_den", "ptob","biomass","production")) {
+  stop("mod argument must be 'sample_den' 'ptob', 'biomass', or 'production'")
+}
+message("[", Sys.time(), "] Model arguement: ",mod_arg," extracted")
+
+
+message("[", Sys.time(), "] Selecting input file for array task ", task,
+        " and mod arg: ",mod_arg)
 file_name <- list.files(
   input_dir,
-  pattern = "\\.rds$",
+  # pattern = "\\.rds$",
+  pattern = paste0(mod_arg,".*\\.rds$"),
   full.names = TRUE
 )[task]
 message("[", Sys.time(), "] Input file: ", file_name)
+
 
 # Load in data
 message("[", Sys.time(), "] Loading data...")
 data <- readRDS(file_name)
 message("[", Sys.time(), "] Data loaded")
 
-# Set selected value for m
-message("[", Sys.time(), "] Setting M value based on shell argument...")
-arg <- commandArgs(trailingOnly = TRUE)
-if (length(arg) < 1) {
-  stop("Missing required argument M")
+# Select model type
+
+message(
+  "[", Sys.time(), "] Selecting model type based on response arg: ", mod_arg
+  )
+if(mod_arg %in% c("sample_den","biomass","production")) {
+  mod_name <- "tweedie_mvn_regyear_effects.stan" 
+  }
+
+if(mod_arg == "ptob"){
+  mod_name <- "hurdle_mvn_regyear_effects.stan"
 }
-if (length(arg) > 1) {
-  stop("Multiple values supplied for argument M")
+message("[", Sys.time(), "] ",mod_name," selected")
+
+if(grepl("tweedie",mod_name)) {
+  # Set selected value for m
+  message("[", Sys.time(), "] Setting M value based on shell argument...")
+  m_arg <- arg[2]
+  if (is.na(m_arg)) {
+    stop("Missing required argument M")
+  }
+  # if (length(m_arg) > 1) {
+  #   stop("Multiple values supplied for argument M")
+  # }
+  M <- as.numeric(m_arg)
+  if (is.na(M)) {
+    stop("Argument 'value' must be numeric")
+  }
+  data$M <- M
+  message("[", Sys.time(), "] Set M in data to ", M)
 }
-M <- as.numeric(arg)
-if (is.na(arg)) {
-  stop("Argument 'value' must be numeric")
-}
-data$M <- M
-message("[", Sys.time(), "] Set M in data to ", M)
 
 
 # Run models  ------------------------------------------------------------------
 
 # Load in model
-mod_name <- "tweedie_mvn_regyear_effects.stan"
+# mod_name <- "tweedie_mvn_regyear_effects.stan"
 message("[", Sys.time(), "] Compiling/loading Stan model...")
 mod <- cmdstan_model(file.path(mod_dir,mod_name))
 message("[", Sys.time(), "] Stan model ready")
@@ -137,19 +172,29 @@ if (issues == 0) {
   message("Issues detected:")
   message("  Rhat > 1.1: ", high_rhat)
   message("  Divergent transitions: ", div)
-  message(" Max tree depth exceeded: ", tree)
+  message("  Max tree depth exceeded: ", tree)
   message("============================================================")
 }
 
 # Export  ----------------------------------------------------------------------
 
 # Name file and export
-out_name <-  file_name |>
-  gsub("hpc/data",out_dir,x=_) |>
-  gsub(
-    "_input_data",
-    paste0("_stan_out_M",M),
-    x=_)
+
+if(grepl("tweedie",mod_name)) {
+  out_name <-  file_name |>
+    gsub("hpc/data",out_dir,x=_) |>
+    gsub(
+      "_input_data",
+      paste0("_stan_out_M",M),
+      x=_)
+} else {
+  out_name <-  file_name |>
+    gsub("hpc/data",out_dir,x=_) |>
+    gsub(
+      "_input_data",
+      "_stan_out",
+      x=_)
+}
 
 out$save_object(out_name)
 message("[", Sys.time(), "] Model successfully saved:")
